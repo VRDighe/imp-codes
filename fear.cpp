4d9fcd0b0802ce05f05f76214e99706a01105ceb
vaibhav dighe <vaibhavdighes7@gmail.com>
2019-03-01 00:16:25 +0530

    simple but tricky

---
+++ fear.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+int no,i,j,m=0;
+cout<<"Enter the no"<<endl;
+cin>>no;
+
+int array[no];
+i=no;
+while(i>=1)
+{
+array[m]=i;
+m++;
+i=i-2;
+}
+i=no-1;
+while(i>=1)
+ {
+ array[m]=i;
+  m++;
+ i=i-2;
+ }
+
+
+for(int k=0;k<no;k++)
+ {
+ cout<<array[k]<<"\t";
+ }
+cout<<endl;
+return 0; 
+}
+ 
